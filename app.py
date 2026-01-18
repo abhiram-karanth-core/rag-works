580098a27daeeb15e99bf259868e45583bc68a2c
Abhiram-Dev2004 <abhiramkaranth22@gmail.com>
2026-01-18 12:02:46 +0530

    switch db from SQLite to PostegreSQL

--- app.py
+++ app.py
@@ -17,9 +17,12 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+database_url = os.getenv("DATABASE_URL")
+if not database_url:
+    raise RuntimeError("DATABASE_URL is not set")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY", "super-secret-key-change-me")  # Change this!
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
