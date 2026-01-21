ae40885499479e54a40fafb9355e7337a4ca0978
Abhiram-Dev2004 <abhiramkaranth22@gmail.com>
2026-01-21 23:20:21 +0530

    update db model'

--- app.py
+++ app.py
@@ -37,8 +37,16 @@ jwt = JWTManager(app)
 # ---------------- MODELS ----------------
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+
+    provider = db.Column(db.String(20), default="local")
+    provider_sub = db.Column(db.String(120), nullable=True)
+
+    password_hash = db.Column(db.String(255), nullable=True)
+
+
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
